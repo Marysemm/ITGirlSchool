c8113c63d0b5a4d2eecc82dd80d3ed65d13459ca
Marysemm <a.mascha.m@yandex.ru>
2023-02-15 17:52:29 +0300

    hw week28 added state

--- week27app/src/components/Rates.jsx
+++ week27app/src/components/Rates.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./style.module.scss";
 
 function Rates(props) {
-    const chosen = props.selected ? 'selected' : '';
+    const [isSelected, setSelected] = useState(false);
+
+    const handleChange = () => {
+        setSelected(!isSelected);
+    }
+
     return (
-        <div className={`${style.card} ${style[chosen]}`}>
+        <div className={`${style.card} ${style[isSelected ? 'selected' : '']}`}>
             <h2 className={`${style.card__title} ${style[props.color]}`}>{props.rateName}</h2>
             <div className={`${style.card__rate} ${style[props.color]}`}>
                 <span className={style.rateValue}>руб </span>
@@ -13,7 +18,10 @@ function Rates(props) {
             </div>
             <p className={style.card__speed}>до {props.condition} Мбит/сек</p>
             <p className={style.card__detailes}>Объем включенного трафика не ограничен</p>
-        </div >
+            <button className={`${style[isSelected ? 'btn_selected' : 'btn_not-selected']}`} onClick={handleChange}>
+                {isSelected ? 'Выбрано!' : 'Выбрать'}
+            </button>
+        </div>
     );
 }
 
