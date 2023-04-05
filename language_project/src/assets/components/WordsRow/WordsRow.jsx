b3c5ae336844ca8848dc062a593b9e7e332c8f6c
Marysemm <a.mascha.m@yandex.ru>
2023-04-05 15:16:04 +0300

    added validation

--- language_project/src/assets/components/WordsRow/WordsRow.jsx
+++ language_project/src/assets/components/WordsRow/WordsRow.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import CSSModules from "react-css-modules";
 import style from "./style.module.scss";
 
@@ -10,64 +10,124 @@ function WordsRow(props) {
         russian: props.russian,
         id: props.id,
     })
+    const [errorEnglish, setErrorEnglish] = useState(false);
+    const [errorTranscription, setErrorTranscription] = useState(false);
+    const [errorRussian, setErrorRussian] = useState(false);
+    const [formValid, setFormValid] = useState(false);
+
+    useEffect(() => {
+        if (errorEnglish || errorTranscription || errorRussian) {
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    }, [errorEnglish, errorTranscription, errorRussian])
 
     const handleEdit = () => {
         setEditForm(!editForm);
     }
 
+    const checkValidation = () => {
+        if (defaultRow.english === "") {
+            setErrorEnglish(true);
+        }
+        if (defaultRow.transcription === "") {
+            setErrorTranscription(true);
+        }
+        if (defaultRow.russian === "") {
+            setErrorRussian(true);
+        }
+        if (defaultRow.english !== "" && defaultRow.transcription !== "" && defaultRow.russian !== "") {
+            setEditForm(!editForm);
+            console.log(props);
+        }
+
+    }
+
     const handleChange = (e) => {
         editDefaultRow({ ...defaultRow, [e.target.name]: e.target.value.toLowerCase() })
+
+        if (e.target.value.match(/[0-9]/)) {
+            alert("Пожалуйста, вводите только буквы")
+        }
+
+        if (defaultRow.english !== "") {
+            setErrorEnglish(false);
+        }
+        if (defaultRow.transcription !== "") {
+            setErrorTranscription(false);
+        }
+        if (defaultRow.russian !== "") {
+            setErrorRussian(false);
+        }
     }
 
     return (
         <Fragment>
-            {
-                editForm ? (
-                    <tbody key={props.id} >
-                        <tr>
-                            <td>{props.english}</td>
-                            <td>{props.transcription}</td>
-                            <td>{props.russian}</td>
-                            <td>
-                                <button styleName="button__edit" onClick={handleEdit}>Edit</button>
-                                <button styleName="button__delete">Delete</button>
-                            </td>
-                        </tr>
-                    </tbody>)
-                    :
-                    (
-                        <tbody key={props.id}>
-                            <tr>
-                                <td>
+            <tbody key={props.id} >
+                <tr>
+                    <td>
+                        {
+                            editForm ? (
+                                <div>{props.english}</div>)
+                                :
+                                (
                                     <input type="text"
                                         name="english"
+                                        styleName={errorEnglish ? "input__error" : "input__edit"}
                                         required="required"
                                         value={defaultRow.english}
                                         onChange={handleChange} />
-                                </td>
-                                <td>
+                                )
+                        }
+                    </td>
+                    <td>
+                        {
+                            editForm ? (
+                                <div>{props.transcription}</div>
+                            )
+                                :
+                                (
                                     <input type="text"
                                         name="transcription"
+                                        styleName={errorTranscription ? "input__error" : "input__edit"}
                                         required="required"
                                         value={defaultRow.transcription}
                                         onChange={handleChange} />
-                                </td>
-                                <td>
+                                )
+                        }
+                    </td>
+                    <td>
+                        {
+                            editForm ? (
+                                <div>{props.russian}</div>
+                            )
+                                :
+                                (
                                     <input type="text"
                                         name="russian"
+                                        styleName={errorRussian ? "input__error" : "input__edit"}
                                         required="required"
                                         value={defaultRow.russian}
                                         onChange={handleChange} />
-                                </td>
-                                <td>
-                                    <button styleName="button__cancel" onClick={handleEdit}>Cancel</button>
-                                    <button styleName="button__delete">Delete</button>
-                                </td>
-                            </tr>
-                        </tbody>)
-            }
+                                )
+                        }
+                    </td>
+                    <td>
+                        {
+                            editForm ? (
+                                <button styleName="button__edit" onClick={handleEdit}>Edit</button>
+                            )
+                                :
+                                (
+                                    <button styleName="button__cancel" disabled={!formValid} onClick={checkValidation}>Save</button>
+                                )
+                        }
+                        <button styleName="button__delete">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
         </Fragment>
     )
-}
-
+};
 export default CSSModules(WordsRow, style);
