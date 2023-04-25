d7fb272fa60f943fae034486285f1e4e99a2e031
Mariya <108333401+Marysemm@users.noreply.github.com>
2023-04-25 21:04:29 +0300

    fixed small error

--- language_project/src/assets/components/WordCollection/WordCollection.jsx
+++ language_project/src/assets/components/WordCollection/WordCollection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { globalContext } from "../../Context/MyContext";
 import CSSModules from 'react-css-modules';
 import Word from '../Word/Word';
